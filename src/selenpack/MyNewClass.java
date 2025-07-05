296788f38a1589752126c4bc5d0222dd071b5359
RameshriSalian18 <rameshrisalian90@gmail.com>
2025-07-05 13:43:56 +0530

    hello

---
+++ src/selenpack/MyNewClass.java
@@ -0,0 +1,11 @@
+package selenpack;
+
+public class MyNewClass 
+{
+public static void main(String[] args) 
+{
+	System.out.println("hii");
+	System.out.println("done");
+		System.out.println("hhhhh");
+}
+}
